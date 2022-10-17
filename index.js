267ba29dc99d50d3c1dd10f8191812404217d75e
Thiago Nascimento <thiago.nascimento.ti@gmail.com>
2022-10-16 22:30:15 -0300

    Refactoring code

--- index.js
+++ index.js
@@ -9,140 +9,80 @@ const server = express();
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser())
 
-server.get("/", (req, res) => {
-  const html = jetpack.read(__dirname+"/view/index.html");
-  const database = JSON.parse(jetpack.read("./database.json"));
-  const cartaz = database.cartaz;
-  let result = "";
+function render(htmlPath, values = {}) {
+  const html = jetpack.read(__dirname+htmlPath);
+  return Eta.render(html, values);
+}
 
-  const listagem = []
-  for (slug in cartaz) {
-  const filme = cartaz[slug];
-  listagem.push({ ...filme, slug });
-  }
-  const formattedHtml = Eta.render(html, { listagem, result });
+server.get("/", (req, res) => {
+  const listagem = getAllMovies();
+  const formattedHtml = render("/view/index.html", { listagem, result: "" })
   res.send(formattedHtml);
 })
 
 server.post("/search", (req, res) => {
-  const html = jetpack.read(__dirname+"/view/movie.html");
-  const htmlHomePage = jetpack.read(__dirname+"/view/index.html");
-  const database = JSON.parse(jetpack.read("./database.json"));
   const param = req.body.searchInput;
 
-  let callMovie = searchMovie(param, database);
+  const callMovieHash = searchMovieHash(param);
+  if(callMovieHash) {
+    const filme = getMovieById(callMovieHash);
 
-  if(callMovie) {
-    const hash = callMovie;
-    const filme = database.cartaz[hash];
-
-    const formattedHtml = Eta.render( html, filme );
+    const formattedHtml = render("/view/movie.html", filme);
     res.send(formattedHtml);
   } else {
-    const cartaz = database.cartaz;
-    const listagem = []
-    for (slug in cartaz) {
-    const filme = cartaz[slug];
-    listagem.push({ ...filme, slug });
-    }
+    const listagem = getAllMovies();
 
-    var result = 'O filme "' + param + '" não existe em nossa biblioteca';
-    const formattedHtml = Eta.render( htmlHomePage, { listagem, result });
+    const formattedHtml = render("/view/index.html", { 
+      listagem, 
+      result: 'O filme "' + param + '" não existe em nossa biblioteca' 
+    });
     res.send(formattedHtml);
   }
 })
 
-server.get("/newMovie", (req, res) => {
-  const html = jetpack.read(__dirname+"/view/newMovie.html");
-  const feedback = "";
-  const formattedHtml = Eta.render( html, feedback);
+server.get("/movie/new", (req, res) => {
+  const formattedHtml = render("/view/newMovie.html", "");
   res.send(formattedHtml);
 })
 
-server.post("/newMovie", (req, res) => {
-  const html = jetpack.read(__dirname+"/view/newMovie.html");
-  const database = JSON.parse(jetpack.read("./database.json"));
+server.post("/movie/new", (req, res) => {
   const movie = req.body;
 
-  let feedback = titleCheckDataBase(database, movie);
-
-  if(database.cartaz[movie.idMovie]) {
-    if(feedback) {
-      if(feedback === true) {
-        database.cartaz[movie.idMovie] = {
-          title: movie.nameMovie,
-          url: movie.imageMovie,
-          release: movie.ageMovie,
-          id: movie.idMovie,
-        }
-        jetpack.write("./database.json", database);
-        res.redirect("/")
-      } else {
-        feedback = 'O filme "' + movie.nameMovie + '" ja existe em nossa biblioteca.';
-        const formattedHtml = Eta.render( html, feedback);
-        res.send(formattedHtml);
-      }
-    } else {
-      database.cartaz[movie.idMovie] = {
-        title: movie.nameMovie,
-        url: movie.imageMovie,
-        release: movie.ageMovie,
-        id: movie.idMovie,
-      }
-      jetpack.write("./database.json", database);
-      res.redirect("/")
+  const savedMovie = getMovieByName(movie.nameMovie)
+  if (savedMovie && savedMovie.id != movie.idMovie) {
+    res.send(renderExistMovie());
+    return;
   }
-  } else if(feedback) {
-    feedback = 'O filme "' + feedback + '" ja existe em nossa biblioteca.';
-    const formattedHtml = Eta.render( html, feedback);
-    res.send(formattedHtml);
+
+  const toEditMovie = getMovieById(movie.idMovie);
+  if (toEditMovie) {
+    saveMovie(movie)
+    res.redirect("/")
   } else {
-  req.body.idMovie = uuidv4();
-  const id = req.body.idMovie;
-  const databaseMovie = database.cartaz[id] || {}
-  database.cartaz[id] = {
-    ...databaseMovie,
-    title: movie.nameMovie,
-    url: movie.imageMovie,
-    release: movie.ageMovie,
-    id,
+    createMovie(movie);
+    res.redirect("/");
   }
-  jetpack.write("./database.json", database);
-
-  res.redirect("/");
-}
 })
 
 server.get("/movie/:slug", (req, res) => {
-  const database = JSON.parse(jetpack.read("./database.json"));
-  const html = jetpack.read(__dirname+"/view/movie.html");
-  const slug = req.params.slug;
-  const filme = database.cartaz[slug];
+  const filme = getMovieById(req.params.slug);
 
   if (!filme) {
-    const htmlNotFound = jetpack.read(__dirname+"/view/movieNotFound.html");
-    const formattedHtml = Eta.render(htmlNotFound, slug)
+    const formattedHtml = render("/view/movieNotFound.html", slug)
     res.status(404).send(formattedHtml);
-  } else if (req.headers.accept === "application/json") {
-    res.json({ ...filme, slug });
   } else {
-    const formattedHtml = Eta.render(html, { ...filme, slug });
+    const formattedHtml = render("/view/movie.html", { ...filme, slug })
     res.send(formattedHtml)
   }
 });
 
-server.get("/newMovie/:slug", (req, res) => {
-  const slug = req.params.slug;
-  const database = JSON.parse(jetpack.read('./database.json'));
-  const filme = database.cartaz[slug];
-  const html = jetpack.read(__dirname+"/view/editMovie.html");
-  let feedback = "";
-  const formattedHtml = Eta.render( html, { ...filme, slug, feedback });
-
+server.get("/movie/:slug/edit", (req, res) => {
+  const filme = getMovieById(req.params.slug)
+  const formattedHtml = render("/view/editMovie.html", { ...filme, slug, feedback: "" });
   res.send(formattedHtml);
 })
 
-server.get("/movieDelete/:slug", (req, res) => {
+server.get("/movie/:slug/delete", (req, res) => {
   const slug = req.params.slug;
   const database = JSON.parse(jetpack.read('./database.json'));
 
@@ -151,31 +91,74 @@ server.get("/movieDelete/:slug", (req, res) => {
   res.redirect("/");
 })
 
-server.get("/movieNotFound", (req, res) => {
-  const html = jetpack.read(__dirname+"/view/newMovie.html");
-  const formattedHtml = Eta.render( html );
+server.get("/movie-not-found", (req, res) => {
+  const formattedHtml = render("/view/newMovie.html")
   res.send(formattedHtml);
 })
 
-function searchMovie(param, database) {
+function saveMovie(movie) {
+  const database = JSON.parse(jetpack.read("./database.json"));
+  database.cartaz[movie.idMovie] = {
+    title: movie.nameMovie,
+    url: movie.imageMovie,
+    release: movie.ageMovie,
+    id: movie.idMovie,
+  }
+  jetpack.write("./database.json", database);
+}
+
+function createMovie(movie) {
+  const id = uuidv4();
+  const database = JSON.parse(jetpack.read("./database.json"));
+  database.cartaz[id] = {
+    title: movie.nameMovie,
+    url: movie.imageMovie,
+    release: movie.ageMovie,
+    id,
+  }
+  jetpack.write("./database.json", database);
+} 
+
+function renderExistMovie() {
+  return render("/view/newMovie.html", 'O filme "' + movie.nameMovie + '" ja existe em nossa biblioteca.')
+}
+
+function searchMovieHash(param) {
+  const database = JSON.parse(jetpack.read("./database.json"));
   for (let filme in database.cartaz) {
-    if(database.cartaz[filme].title.toLowerCase() == param.toLowerCase()) {
+    if (database.cartaz[filme].title.toLowerCase() == param.toLowerCase()) {
       return filme;
     }
   }
 }
 
-function titleCheckDataBase(database, movie) {
-  for(let filme in database.cartaz) {
-    if(database.cartaz[filme].title.toLowerCase() === movie.nameMovie.toLowerCase()) {
-      if(filme === movie.idMovie) {
-        return true;
-      } else {
-        return database.cartaz[filme].title
-      };
+function getAllMovies() {
+  const database = JSON.parse(jetpack.read("./database.json"));
+  const cartaz = database.cartaz;
+
+  const movies = []
+  for (slug in cartaz) {
+    const movie = cartaz[slug];
+    movies.push({ ...movie, slug });
+  }
+  return movies;
+}
+
+function getMovieByName(name) {
+  const database = JSON.parse(jetpack.read("./database.json"));
+  for(let hash in database.cartaz) {
+    if (database.cartaz[hash].title.toLowerCase() === name.toLowerCase()) {
+      return database.cartaz[hash]
     }
   }
+  return null;
+}
+
+function getMovieById(idMovie) {
+  const database = JSON.parse(jetpack.read("./database.json"));
+  return database.cartaz[idMovie];
 }
 
 server.listen(3000, () => {
+  console.log("server up", __dirname)
 });
